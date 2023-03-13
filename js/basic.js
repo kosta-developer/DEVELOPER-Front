e374e7e03697d905344e3916d0fc2903cbda918b
nowij0 <moonone470@gmail.com>
2023-03-13 22:58:43 +0900

    no message

--- js/basic.js
+++ js/basic.js
@@ -1,6 +1,6 @@
 //지원
-let backURL = "http://192.168.0.33:8888/developer/";
-let frontURL = "http://192.168.0.33:5500/html/";
+let backURL = "http://192.168.0.28:8888/developer/";
+let frontURL = "http://192.168.0.28:5500/html/";
 
 $(() => {
     showMenuAtLogouted()
